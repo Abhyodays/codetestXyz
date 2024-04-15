cbb69a8fdaefee8a723a17c553f9e4ea4143196d
abhyodaysingh <abhyoday.singh@nagarro.com>
2024-04-16 01:00:02 +0530

    some mod

--- ConsoleApp1/Services/Calculation.cs
+++ ConsoleApp1/Services/Calculation.cs
@@ -4,11 +4,14 @@ namespace ConsoleApp1.Services
     {
         public static bool IsPrime(int num)
         {
-            if (num < 2) return false;
+            if (num < 2)
+            {
+                return false;
+            }
             int i = 2;
             while (i <= num / i)
             {
-                if (num % i == 0) return false;
+                if (num % i == 0) { return false; };
                 i++;
             }
             return true;
@@ -16,7 +19,8 @@ namespace ConsoleApp1.Services
 
         public static bool IsEven(int num)
         {
-            if (num % 2 == 0) return false;
+            if (num % 2 == 0)
+                return false;
             return true;
         }
     }
